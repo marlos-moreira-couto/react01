726b487cf1c4b12ebd8428c7ef82fd387fb32d69
Marlos Couto <marlos@ilog.com.br>
2022-02-27 16:44:40 -0300

    React — Criando o primeiro component — Plano de aula III

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Button from "./components/Button";
 
 class App extends React.Component {
     render() {
         return (
             <h1>
                 Hello React!!
+
+                <Button/>
             </h1>
         );
     }
